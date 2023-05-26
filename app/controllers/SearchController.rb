06bfb25856750e2c1a71fb4ef8e67ed9242d7983
Boston Lowrey <bostonlowrey@gmail.com>
2023-05-26 17:13:37 -0400

    feat: added the functionality for the search index page

--- app/controllers/SearchController.rb
+++ app/controllers/SearchController.rb
@@ -5,9 +5,10 @@ class SearchController < ApplicationController
     conn = Faraday.new(url: "https://last-airbender-api.fly.dev/api/v1/") do |faraday|
     end
       
-    response = conn.get("characters?affiliation=#{nation}")
+    response = conn.get("characters?affiliation=#{nation}", perPage: 200)
 
     json = JSON.parse(response.body, symbolize_names: true)
-    @characters = json
+    @characters = json.take(25)
+    @all_nation_members = json.size
   end
 end
