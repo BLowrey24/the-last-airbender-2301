8d7f3beb7136f6c1593431bb7355deba3af2a1c3
Boston Lowrey <bostonlowrey@gmail.com>
2023-05-26 15:31:38 -0400

    test: for selecting Fire Nation and Searching For Members button

---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'Welcome page', type: :feature do
+  describe 'when is visit /' do
+    it 'and I select fire nation from the select field and click seach for members I should now be on the /search page' do
+      visit '/'
+
+      select 'Fire Nation', from: 'nation'
+      click_button 'Search For Members'
+      expect(current_path).to eq('/search')
+    end
+  end
+end
